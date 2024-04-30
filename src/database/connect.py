00a057e7e82ad3855c551ea11744b8ce65fa9bc1
DawsonPloudre <125403735+DawsonPloudre@users.noreply.github.com>
2024-04-30 16:01:05 -0500

    Commented old code in case need to rollback

--- src/database/connect.py
+++ src/database/connect.py
@@ -64,3 +64,25 @@ if __name__ == '__main__':
        # function_name = 'your_function_name'
        # if function_name in python_functions:
        # result = python_functions[function_name]()
+
+"""
+import psycopg2
+from src.database.config import load_config
+
+
+def connect(config):
+    Connect to the PostgreSQL database server
+    try:
+        # connecting to the PostgreSQL server
+        with psycopg2.connect(**config) as conn:
+            print('Connected to the PostgreSQL server.')
+            return conn
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
+
+
+if __name__ == '__main__':
+    config = load_config()
+    connect(config)
+
+"""
